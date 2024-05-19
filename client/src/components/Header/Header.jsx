74d54ef29c6e8c2843c165d67597bc202f87a450
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-05-19 19:22:46 +0600

    navbar created

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./header.scss"
+
+function Header() {
+  return (
+    <header className="header">
+        <div className="container">
+            <div className="header__inner">
+                <Link className="header__logo logo" to="/">CAFE.</Link>
+                <nav className="navbar header__navbar">
+                  <ul className="navbar__list">
+                    <li className="navbar__item">
+                      <Link className="navbar__link" to="/about">О нас</Link>
+                    </li>
+                    <li className="navbar__item">
+                      <Link className="navbar__link" to="/menu">Меню</Link>
+                    </li>
+                    <li className="navbar__item">
+                      <Link className="navbar__link" to="/book">Забронировать столик</Link>
+                    </li>
+                    <li className="navbar__item">
+                      <Link className="navbar__link" to="/contacts">Контакты</Link>
+                    </li>
+                  </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
